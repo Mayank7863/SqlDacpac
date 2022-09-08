d796d321571a4de62eeb01fc41369bb066cb236b
Mayank7863 <110805608+Mayank7863@users.noreply.github.com>
2022-09-08 12:55:01 +0530

    Update Customers.sql

--- CustomerDB/dbo/Tables/Customers.sql
+++ CustomerDB/dbo/Tables/Customers.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Customers] (
     [Id]        INT            IDENTITY (1, 1) NOT NULL,
-    [Name]      NVARCHAR (500) NOT NULL,
+    [Name]      NVARCHAR (700) NOT NULL,
     [CreatedBy] NVARCHAR (250) NULL,
     [CreatedOn] DATETIME       CONSTRAINT [DF_Customer_CreatedOn] DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED ([Id] ASC)
